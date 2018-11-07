f6c44b798be0b084b80a4aef90ca9abfd26cc666
Theo Brigitte <theo.brigitte@gmail.com>
2018-11-07 05:11:23 +0100

    replace list with search

--- gile.go
+++ gile.go
@@ -1,4 +1,4 @@
-package main // import "github.com/TheoBrigitte/gile"
+package main
 
 import (
 	"context"
@@ -10,29 +10,10 @@ import (
 	"golang.org/x/oauth2"
 )
 
-func die(v ...interface{}) {
-	fmt.Print(v)
-	os.Exit(1)
-}
-
-func printRepos(repos []*github.Repository) {
-	for _, r := range repos {
-		fmt.Println(*r.Name)
-	}
-}
-
-func listByOrg(client *github.Client, org string, ctx context.Context, next int) ([]*github.Repository, *github.Response, error) {
-	opt := &github.RepositoryListByOrgOptions{
-		ListOptions: github.ListOptions{
-			PerPage: 100,
-			Page:    next,
-		},
-	}
-	return client.Repositories.ListByOrg(ctx, org, opt)
-}
-
 func main() {
-	org := flag.String("org", "", "github organization")
+	org := flag.String("org", "", "github organization name")
+	query := flag.String("query", "", "search query")
+	size := flag.Int("size", 10, "number of items to return")
 	flag.Parse()
 
 	ctx := context.Background()
@@ -43,48 +24,22 @@ func main() {
 
 	client := github.NewClient(tc)
 
-	// list all repositories for the authenticated user
-	/*repos, _, err := client.Repositories.List(ctx, "", nil)
+	if *org != "" {
+		*query = fmt.Sprintf("org:%s %s", *org, *query)
+	}
+
+	opt := &github.SearchOptions{
+		ListOptions: github.ListOptions{
+			PerPage: *size,
+		},
+	}
+	results, _, err := client.Search.Repositories(ctx, *query, opt)
 	if err != nil {
-		die(err)
-	}*/
-
-	// first batch
-	var next int
-	var allRepos []*github.Repository
-	repos, resp, err := listByOrg(client, *org, ctx, next)
-	if err != nil {
-		die(err)
-	}
-	allRepos = append(allRepos, repos...)
-
-	// the rest in parallel
-	results := make(chan []*github.Repository)
-	errors := make(chan error)
-
-	for i := resp.NextPage; i <= resp.LastPage; i++ {
-		//repos, resp, err := client.Repositories.ListByOrg(ctx, *org, opt)
-		go func(i int) {
-			repos, _, err := listByOrg(client, *org, ctx, i)
-			if err != nil {
-				errors <- err
-			} else {
-				results <- repos
-			}
-		}(i)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	for i := resp.NextPage; i <= resp.LastPage; i++ {
-		select {
-		case err := <-errors:
-			die(err)
-		default:
-		}
-
-		res := <-results
-		allRepos = append(allRepos, res...)
+	for _, r := range results.Repositories {
+		fmt.Println(*r.Name)
 	}
-
-	printRepos(allRepos)
-	//fmt.Printf("%d repositories\n", len(allRepos))
 }
